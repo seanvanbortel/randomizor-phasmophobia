d81a8512509e641bbd57a7ec2714813e8a485be4
Sean Van Bortel <sean.vanbortel@student.ap.be>
2020-12-07 16:42:35 +0100

    Responsive updated, added array to frontend list

--- views/script.js
+++ views/script.js
@@ -4,7 +4,8 @@ var lightsArray = ['Candle', 'Flashlight', 'Strong Flashlight'];
 var equipmentArray = ['EMF-reader', 'Spirit Box', 'Ghost Writing Book', 'Photo Camera', 'Video Camera', 'Crucifix', 'UV-flashlight', 'Head Mounted Camera', 'Lighter', 'Smudge Sticks', 'Glow Sticks', 'Infrared Light Sensor', 'Motion Sensor', 'Sound Sensor', 'Salt', 'Parabolic Microphone', 'Sanity Pills', 'Thermometer', 'Tripod'];
 
 let button = document.querySelector('button');
-let result = document.querySelector('div');
+let result = document.getElementById("randomizer");
+let resultOnPage = document.getElementById("result");
 
 // FUNCTIONS
 randomNumber = (min, max) => {
@@ -15,52 +16,69 @@ randomNumber = (min, max) => {
 }
 
 var randomizeMap = () => {
-    // var temp = [];
     var counter = 0;
     setInterval(() => {
         for (let i = 0; i < mapArray.length; i++) {
-            if (counter === 1000) {
-                break;
-            } else {
+            if (counter === 50) {
+                clearInterval();
+            }
+            if (counter < 50) {
                 let index = randomNumber(0, mapArray.length - 1);
                 result.innerText = mapArray[index];
                 counter++;
             }
-            console.log(i);
         }
-    }, 10);
-    // result = temp.push(result.innerText).toString();
-    // console.log(temp);
+    }, 20);
 }
+
+
+
+// result = temp.push(result.innerText).toString();
+// console.log(temp);
 
 var randomizeLight = () => {
     var counter = 0;
-    var interval = setInterval(() => {
+    setInterval(() => {
         for (let i = 0; i < lightsArray.length; i++) {
-            if (counter === 500) {
-                clearInterval(interval);
-            } else {
+            if (counter === 300) {
+                clearInterval();
+            }
+            if (counter < 300) {
                 let index = randomNumber(0, lightsArray.length - 1);
                 result.innerText = lightsArray[index];
                 counter++;
             }
-            console.log(i);
         }
-    }, 10);
+    }, 20);
 }
+
+var temp = [];
 
 var randomizeEquipment = () => {
     var counter = 0;
-    var interval = setInterval(() => {
+    setInterval(() => {
         for (let i = 0; i < equipmentArray.length; i++) {
-            if (counter === 3000) {
-                clearInterval(interval);
-            } else {
+            if (counter === 1000) {
+                clearInterval();
+            }
+            if (counter < 1000) {
                 let index = randomNumber(0, equipmentArray.length - 1);
                 result.innerText = equipmentArray[index];
                 counter++;
             }
-            console.log(i);
         }
-    }, 10);
+    }, 20);
+    temp.push(result.innerText);
+    temp.forEach(element => {
+        for (let i = 0; i < temp.length; i++) {
+            if (temp[0] == "Press the button") {
+                temp.shift();
+            }
+            resultOnPage.innerHTML = temp[i] + '<br>' + temp[i - 1] + '<br>' + temp[i - 2] + '<br>';
+        }
+
+        console.log(element);
+    });
+    console.log("Counter: " + counter + " Result: " + result.innerText + " Temp: " + temp);
+
 }
